e057fc82d50512342dbe99b82855fe46482f5464
Ezedélio Garcia <dinog165@gmail.com>
2023-04-13 01:25:28 +0100

    update

--- js/Configurar/Menu.js
+++ js/Configurar/Menu.js
@@ -1,35 +1,28 @@
-let lCandidato = document.getElementById("lista-candidatos");
-let lEleitor = document.getElementById("lista-eleitores");
-let rCandidato = document.getElementById("registrar-candidato");
-let rEleitor = document.getElementById("registrar-eleitor");
+let menu_candidato = document.getElementById("menu-candidato");
+let menu_eleitor = document.getElementById("menu-eleitor");
 
-lCandidato.style.display = "block";
+let candidatos = document.getElementById("candidatos");
+let eleitores = document.getElementById("eleitores");
 
-const showLCandidato = () => {
-    lCandidato.style.display = "block";
-    lEleitor.style.display = "none";
-    rCandidato.style.display = "none";
-    rEleitor.style.display = "none";
+let form_eleitor = document.getElementById("form-candidato");
+
+menu_candidato.classList.add("active");
+candidatos.style.display = "block";
+
+const showCandidatos = () => {
+    menu_candidato.classList.add("active");
+    menu_eleitor.classList.remove("active");
+    candidatos.style.display = "block";
+    eleitores.style.display = "none";
 }
 
-const showLEleitor = () => {
-    lCandidato.style.display = "none";
-    lEleitor.style.display = "block";
-    rCandidato.style.display = "none";
-    rEleitor.style.display = "none";
+const showEleitores = () => {
+    menu_candidato.classList.remove("active");
+    menu_eleitor.classList.add("active");
+    candidatos.style.display = "none";
+    eleitores.style.display = "block";
 }
 
-const showRCandidato = () => {
-    lCandidato.style.display = "none";
-    lEleitor.style.display = "none";
-    rCandidato.style.display = "block";
-    rEleitor.style.display = "none";
+const showFormEleitor = () => {
+    form_eleitor.style.display = "flex";
 }
-
-const showREleitor = () => {
-    lCandidato.style.display = "none";
-    lEleitor.style.display = "none";
-    rCandidato.style.display = "none";
-    rEleitor.style.display = "block";
-}
-
